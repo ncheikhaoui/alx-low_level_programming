d8d2d3031a362a4e653db04696953afa54f51fb4
ncheikhaoui <n.cheikhaoui1989@gmail.com>
2023-06-26 03:36:21 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 void _putchar (char c); 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +11,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
 #endif 
