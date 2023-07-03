9fd653ce31e5dd0bfc8fc6afd7a2f21acdaa178d
ncheikhaoui <n.cheikhaoui1989@gmail.com>
2023-07-03 06:55:26 -0700

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,26 +1,22 @@
-#include <iostream>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the board to print
+ */
 
-void print_chessboard(char (*a)[8]) {
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            std::cout << a[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-int main() {
-    char chessboard[8][8] = {
-        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
-    };
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
 
-    print_chessboard(chessboard);
+	for (i=0; i < 8; i++ )
+	{
+		for(j=0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+		i++;
+	}
 
-    return 0;
 }
