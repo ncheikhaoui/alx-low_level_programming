6c62f663c8ef739745bfe5bb8b052d3c94857399
ncheikhaoui <n.cheikhaoui1989@gmail.com>
2023-07-05 04:42:43 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = is_palindrome("level");
+	printf("%d\n", r);
+	r = is_palindrome("redder");
+	printf("%d\n", r);
+	r = is_palindrome("holberton");
+	printf("%d\n", r);
+	r = is_palindrome("step on no pets");
+	printf("%d\n", r);
+	return (0);
+}
+
+
+/*
+Write a function that returns 1 if a string is a palindrome and 0 if not.
+
+    Prototype: int is_palindrome(char *s);
+
+Expected Output:
+1
+1
+0
+1
+
+
+
+
+
+int checker(char *str, int len, int count)
+{
+	if (*str != '\0' && count <= len)
+	{
+		printf("%s, %d\n", str, len);
+		if (str[len - 1] == str[count])
+			return (checker(str + 1, len - 1, count + 1));
+		return (1);
+	}
+	return (0);
+}
+ */
