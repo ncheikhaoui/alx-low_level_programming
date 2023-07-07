9dfdb992e6bcd81435e11e6e043c4fed89787a3e
ncheikhaoui <n.cheikhaoui1989@gmail.com>
2023-07-07 04:26:04 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 /**
 * main - prints the min number of coins to make change
 * for an amount of money
@@ -15,19 +13,13 @@ int total, count;
 unsigned int i;
 char *p;
 int cents[] = {25, 10, 5, 2};
-
-
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-
-
 total = strtol(argv[1], &p, 10);
 count = 0;
-
-
 if (!*p)
 {
 while (total > 1)
@@ -49,8 +41,6 @@ else
 printf("Error\n");
 return (1);
 }
-
-
 printf("%d\n", count);
 return (0);
 }
