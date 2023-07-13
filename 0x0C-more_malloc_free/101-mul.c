13265e3eda45f895c8a5ff58d4db08b870fcfd9e
ncheikhaoui <n.cheikhaoui1989@gmail.com>
2023-07-13 15:58:27 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -221,9 +221,9 @@ int main(int argc, char *argv[])
 	for (index = 0; final_prod[index]; index++)
 	{
 		if (final_prod[index] != 'x')
-			_putchar(final_prod[index]);
+		    putchar(final_prod[index]);
 	}
-	_putchar('\n');
+	putchar('\n');
 
 	free(next_prod);
 	free(final_prod);
