7c4553e21b459f5d500483e74f377c5ad5120e92
ncheikhaoui <n.cheikhaoui1989@gmail.com>
2023-08-07 11:44:01 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -83,7 +83,7 @@ void print_class(unsigned char *e_ident)
 		printf("ELF64\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+		printf("<unknown: %x>\n",e_ident[EI_CLASS]);
 	}
 }
 
@@ -117,8 +117,7 @@ void print_data(unsigned char *e_ident)
  *    */
 void print_version(unsigned char *e_ident)
 {
-	 printf(" Version: %d",
-			  e_ident[EI_VERSION]);
+	 printf(" Version: %d",e_ident[EI_VERSION]);
 
 	switch (e_ident[EI_VERSION])
 	{
@@ -182,8 +181,7 @@ void print_osabi(unsigned char *e_ident)
  */
 void print_abi(unsigned char *e_ident)
 {
-	printf(" ABI Version: %d\n",
-		e_ident[EI_ABIVERSION]);
+	printf(" ABI Version: %d\n",e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -231,8 +229,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
-		e_entry = ((e_entry << 8) & 0xFF00FF00) |
-			  ((e_entry >> 8) & 0xFF00FF);
+		e_entry = ((e_entry << 8) & 0xFF00FF00) |((e_entry >> 8) & 0xFF00FF);
 		e_entry = (e_entry << 16) | (e_entry >> 16);
 	}
 
@@ -253,8 +250,7 @@ void close_elf(int elf)
 {
 	if (close(elf) == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", elf);
+		dprintf(STDERR_FILENO,"Error: Can't close fd %d\n", elf);
 		exit(98);
 	}
 }
